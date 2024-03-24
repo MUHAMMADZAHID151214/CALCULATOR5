c8015c1a8fc7339fb3124d5120b27a385f3523d0
Muhammad Zahid <164073545+MUHAMMADZAHID151214@users.noreply.github.com>
2024-03-24 07:58:10 +0500

    my first commit

---
+++ main1.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter  Your First Number : ", type: "number", name: "FirstName" },
+    { message: "Enter Your Second Number : ", type: "number", name: "SecondName" },
+    {
+        message: "Operator : ",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log("Answer = ", answer.FirstName + answer.SecondName);
+}
+else if (answer.operator === "Subtraction") {
+    console.log("Answer = ", answer.FirstName - answer.SecondName);
+}
+else if (answer.operator === "Multiplication") {
+    console.log("Answer = ", answer.FirstName * answer.SecondName);
+}
+else if (answer.operator === "Division") {
+    console.log("Answer = ", answer.FirstName / answer.SecondName);
+}
+else {
+    console.log("Invalid Entry");
+}
